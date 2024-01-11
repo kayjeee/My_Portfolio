d3e61198c6fe04978cfd87adb574a1d2ff83fd12
kayjeee <kagiso.killagram@gmail.com>
2024-01-11 17:19:21 +0200

    fixed some parrts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header id="home">
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Christian Montero</h1>
+        <h1>Kagiso Sebogodi (kayjeee)</h1>
         <h5 className="text-light">Full-Stack Developer</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
